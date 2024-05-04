039df90e1c72e8539a0d48e7c9ac8a4a3818bc29
jpeeicruz <jp.delacruz@kabootekph.com>
2024-05-04 13:27:12 +0800

    fix alish importing module @

---
+++ src/Components/Button.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+interface ButtonProps {
+  label: string;
+  onClick: () => void;
+  className?: string; // Changed 'classNames' to 'className' for consistency with React's prop naming convention
+  disabled?: boolean; // Added 'disabled' prop for disabling the button
+}
+
+const Button: React.FC<ButtonProps> = ({
+  label,
+  onClick,
+  className = '', // Set a default value for className
+  disabled = false, // Set a default value for disabled
+}) => {
+  return (
+    <button
+      type='button'
+      onClick={onClick}
+      className={`rounded-md bg-blue-900 text-white p-2 ${className}`} // Using template literals for better readability
+      disabled={disabled} // Added disabled attribute to the button
+    >
+      {label}
+    </button>
+  );
+};
+
+export default Button;
--- src/Pages/Users/index.tsx
+++ src/Pages/Users/index.tsx
@@ -1,16 +1,23 @@
-import { useState } from 'react'
+import Button from '@/Components/Button'
 
 const Users = () => {
   return (
     <div className='flex flex-col p-5 w-full max-w-screen-2xl'>
 
-      <div className='flex flex-row bg-blue-300'>
-        test
+      <div className='flex flex-row justify-between items-center'>
+        <h2 className='font-bold font-sans'>Users</h2>
+        <div className='flex flex-row'>
 
+          <input
+            type='text'
+            placeholder='Search User'
+            className='w-[700px] shadow-sm p-2 mr-10' />
 
+          <Button
+            onClick={() => console.log('user click')}
+            label="Add User" />
+        </div>
       </div>
-
-
     </div>
   )
 }
