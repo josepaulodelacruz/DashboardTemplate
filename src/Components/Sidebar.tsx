0de4553ba72b4e80d9a5364460458b18174bb55b
jpeeicruz <jp.delacruz@kabootekph.com>
2024-05-07 20:25:36 +0800

    dynamic sidebar menus

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -1,40 +1,41 @@
 import React, { FC, ReactNode } from 'react'
-import { FaLeftLong, FaUsers, FaHouse } from 'react-icons/fa6'
+import { FaLeftLong  } from 'react-icons/fa6'
 import { Link } from 'react-router-dom'
 import '../index.css'
+import {  SidebarMenus } from '@/Constants/StringRoutes'
+
 
 interface SidebarProps {
   isClose?: boolean,
   style?: React.HtmlHTMLAttributes<string>
 }
 
-const Sidebar: FC<SidebarProps> = ({ isClose, style }) => {
+const Sidebar: FC<SidebarProps> = ({ isClose }) => {
   return (
-    <div style={{'backgroundColor': '#202225'}} className={`item bg-primary flex flex-col py-8 h-screen`}>
+    <div style={{ 'backgroundColor': '#202225' }} className={`item bg-primary flex flex-col py-8 h-screen`}>
 
       <ul className='flex h-full flex-col text-white leading-tight overflow-y-auto overflow-x-hidden'>
 
-        <SidebarIcon
-          routePath='/'
-          icon={<FaHouse size={24} />}
-          linkName='Home'
-          isClose={isClose!}
-        />
+        {
+          SidebarMenus.map((item, index) => (
+            <SidebarIcon
+              key={index}
+              routePath={item.path}
+              icon={item.icon}
+              linkName={item.pathName}
+              isClose={isClose!}
+              />
+          ))
+        }
 
-        <SidebarIcon
-          routePath='/users'
-          icon={<FaUsers size={24} />}
-          linkName='User Management'
-          isClose={isClose!}
-        />
       </ul>
 
-      <SidebarIcon 
+      <SidebarIcon
         routePath='/'
-        icon={ <FaLeftLong size={24} color='white' />}
+        icon={<FaLeftLong size={24} color='white' />}
         linkName='LOGOUT'
         isClose={isClose!}
-        />
+      />
 
     </div>
   )
@@ -44,16 +45,17 @@ const Sidebar: FC<SidebarProps> = ({ isClose, style }) => {
 interface SidebarIconProps {
   linkName?: string,
   icon?: ReactNode
-  routePath?: string|null,
-  isClose: boolean
+  routePath?: string | null,
+  isClose: boolean,
+  index?: number 
 }
 
-const SidebarIcon: FC<SidebarIconProps> = ({ linkName, icon, routePath, isClose = false }) => {
+const SidebarIcon: FC<SidebarIconProps> = ({ linkName, icon, routePath, isClose = false, index }) => {
   return (
-    <li className='flex items-center'>
+    <li className='flex items-center' key={index}>
       <Link to={`${routePath}`} className={`flex items-center flex-row gap-7 hover:bg-white hover:bg-opacity-20 rounded-lg flex-1 px-2 mx-2 py-2 transition-colors`}>
         {icon}
-        <span style={{  'fontSize': '12px', 'opacity': isClose ? '1' : '0', 'display': isClose ? 'block' : 'none', 'transitionDelay': isClose ? '0s' : '0.3s' }} className='text-white transition-all whitespace-nowrap font-bold'>{linkName}</span>
+        <span style={{ 'fontSize': '12px', 'opacity': isClose ? '1' : '0', 'display': isClose ? 'block' : 'none', 'transitionDelay': isClose ? '0s' : '0.3s' }} className='text-white transition-all whitespace-nowrap font-bold'>{linkName}</span>
       </Link>
     </li>
   )
--- src/Constants/StringRoutes.tsx
+++ src/Constants/StringRoutes.tsx
@@ -1,8 +1,26 @@
+import { FaHouse, FaUsers } from 'react-icons/fa6'
 
 const RouteModule = {
   USER: '/users',
   ADD_USER: '/users/add-user'
 }
 
-export default RouteModule
+
+const SidebarMenus = [
+  {
+    path: '/',
+    pathName: 'Home',
+    icon: <FaHouse size={24} />
+  },
+  {
+    path: RouteModule.USER,
+    pathName: 'User Management',
+    icon: <FaUsers size={24} />
+  },
+]
+
+export {
+  RouteModule,
+  SidebarMenus
+} 
 
